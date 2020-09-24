084d4421f43f3b435077540caf168b120f124b30
Zak DeBrine <zdebrine@gmail.com>
2020-09-24 13:59:58 -0600

    Implemented hooks in App component

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,19 +1,20 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 import GlobalStyle from './globalStyle';
 
-class App extends React.PureComponent {
-  render() {
-    return (
-      <>
-        <GlobalStyle />
-        <body>
-          <h1> Dangerzone. </h1>
-          <p> Long Live the Scrumdog Billionaires </p>
-          <button type="submit"> Button Large </button>
-        </body>
-      </>
-    );
-  }
+const App = () => {
+  const [count, setCount] = useState(0);
+
+  return (
+    <body>
+      <GlobalStyle />
+      <h1> Dangerzone. </h1>
+      <p> Long Live the Scrumdog Billionaires </p>
+      <p>The count is {count}</p>
+      <button type="submit" className="large-button" onClick={() => setCount(count + 1)}> Button Large </button>
+    </body>
+  );
 }
+
 
 export default App;
--- client/src/test/App.test.js
+++ client/src/test/App.test.js
@@ -2,15 +2,23 @@ import React from 'react';
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import App from '../components/App.jsx';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 
 configure({ adapter: new Adapter() })
 
 
 describe('App', () => {
 
-    it('Renders the app', () => {
-      const wrapper = shallow(<App />);
+    it('renders the page', () => {
+      const wrapper = mount(<App />);
       expect(wrapper).toMatchSnapshot();
+    });
+  });
+
+describe('App', () => {
+
+    it('has a button with className button-large', () => {
+      const wrapper = mount(<App />);
+      expect(wrapper.find('.button-large')).toBeDefined;
     });
   });
