30de836134b8502e0866c956af2cb5be310ee5a8
Zak DeBrine <zdebrine@gmail.com>
2020-09-26 12:51:48 -0600

    Code cleanup

--- client/src/Helpers/GetAverageStarRating.js
+++ client/src/Helpers/GetAverageStarRating.js
@@ -1,5 +1,6 @@
 const getAverageStarRating = (recObj) => {
-  const weightedSum = ((1 * recObj[1]) + (2 * recObj[2]) + (3 * recObj[3]) + (4 * recObj[4]) + (5 * recObj[5]));
+  const weightedSum = ((1 * recObj[1]) + (2 * recObj[2])
+  + (3 * recObj[3]) + (4 * recObj[4]) + (5 * recObj[5]));
   const total = recObj[1] + recObj[2] + recObj[3] + recObj[4] + recObj[5];
   const avgStarVal = (weightedSum / total);
   return avgStarVal.toFixed(1);
