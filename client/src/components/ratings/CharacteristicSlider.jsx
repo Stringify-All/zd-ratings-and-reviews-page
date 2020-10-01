60428295dd27047c46666da1813dcdb6f6b66c36
Zak DeBrine <zdebrine@gmail.com>
2020-10-01 13:42:15 -0600

    Added labels to characteristics sliders

--- client/src/components/ratings/CharacteristicSlider.jsx
+++ client/src/components/ratings/CharacteristicSlider.jsx
@@ -2,28 +2,40 @@ import React, { useState, useEffect } from 'react';
 import Typography from '@material-ui/core/Typography';
 import Slider from '@material-ui/core/Slider';
 
-const ratings = [
-  {
-    value: 0.5,
-    label: 'Too snug',
-  },
-  {
-    value: 2.5,
-    label: 'Just right',
-  },
-  {
-    value: 4.5,
-    label: 'Too big',
-  },
-];
-
 const CharacteristicSlider = (props) => {
-  console.log(props);
   const value = Number(props.characteristic[1].value);
+  const characteristic = props.characteristic[0];
+  const [labels, setLabels] = useState(['Too snug', 'Just right', 'Too big']);
+
+  useEffect(() => {
+    if (characteristic === 'Length') {
+      setLabels(['Too short', 'Perfect', 'Too long']);
+    } else if (characteristic === 'Comfort' || characteristic === 'Quality') {
+      setLabels(['Eh...', '', 'Amazing']);
+    } else {
+      setLabels(['Too snug', 'Just right', 'Too big']);
+    }
+  }, []);
+
+  const ratings = [
+    {
+      value: 0.5,
+      label: labels[0],
+    },
+    {
+      value: 2.5,
+      label: labels[1],
+    },
+    {
+      value: 4.5,
+      label: labels[2],
+    },
+  ];
+
   return (
     <div className="mt-4">
       <Typography id="discrete-slider-always" gutterBottom>
-        {props.characteristic[0]}
+        {characteristic}
       </Typography>
       <Slider
         defaultValue={value}
