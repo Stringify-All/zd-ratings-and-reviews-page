0f4a28e5025dd0f8cc85acf259e7200b08ccca02
Zak DeBrine <zdebrine@gmail.com>
2020-10-01 18:17:24 -0600

    Added correct date format to review entry

---
+++ client/src/Helpers/DateConverter.js
@@ -0,0 +1,12 @@
+const months = ['January', 'Feburary', 'March', 'April', 'May', 'June', 'July', 'August', 'Septemner', 'October', 'November', 'December'];
+
+const dateConverter = (date) => {
+  const id = new Date(date);
+  const day = `${id.getDate()}th`;
+  const month = months[(id.getMonth()) - 1];
+  const year = id.getFullYear();
+
+  return (`${month} ${day}, ${year}`);
+};
+
+export default dateConverter;
