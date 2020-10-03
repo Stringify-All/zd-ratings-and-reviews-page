fbee08014ab9cdfe50ea4c51390f80755b506df1
Zak DeBrine <zdebrine@gmail.com>
2020-10-03 11:03:14 -0600

    Rendering search state after 3 characters are inputted into the searchBar

--- client/src/components/reviews/SearchBar.jsx
+++ client/src/components/reviews/SearchBar.jsx
@@ -1,18 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import OutlinedInput from '@material-ui/core/OutlinedInput';
 import FormControl from '@material-ui/core/FormControl';
 import InputLabel from '@material-ui/core/InputLabel';
 
-const SearchBar = () => (
-  <div>
-    <FormControl fullWidth variant="outlined">
-      <InputLabel>Search</InputLabel>
-      <OutlinedInput
-        id="outlined-adornment-amount"
-        labelWidth={60}
-      />
-    </FormControl>
-  </div>
-);
+const SearchBar = () => {
+  const [input, setInput] = useState(null);
+
+  const handleInput = (event) => {
+    const inputText = event.target.value;
+    if (inputText.length >= 3) {
+      setInput(inputText);
+    } else {
+      setInput(null);
+    }
+  };
+
+  return (
+    <div>
+      <FormControl fullWidth variant="outlined">
+        <InputLabel>Search</InputLabel>
+        <OutlinedInput
+          onChange={handleInput}
+          labelWidth={60}
+        />
+      </FormControl>
+    </div>
+  );
+};
 
 export default SearchBar;
